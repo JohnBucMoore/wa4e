a51649dc8f3433363f9d1a903c65870d090b7a91
JohnBucMoore <JohnBucMoore@gmail.com>
2018-10-06 15:14:00 -0400

    Add files via upload

---
+++ first/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>John Buchanan Moore PHP</title>
+        <meta charset="UTF-8">
+    </head>
+<h1>John Buchanan Moore PHP</h1>
+<p>
+    <?php
+        echo "Hi there.\n";
+        $answer = 6 * 7;
+        echo "The answer is $answer, what was the question again?\n";
+    ?>
+</p>
+<p>Yes another paragraph.</p>
+</html>
---
+++ second/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>John Buchanan Moore PHP</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>John Buchanan Moore PHP</h1>
+    <pre>
+        ___     
+       |\  \    
+       \ \  \   
+     __ \ \  \  
+    |\  \\_\  \ 
+    \ \________\
+     \|________|
+    </pre>
+    <p>
+        <?php
+            print hash('sha256', 'John Buchanan Moore');
+        ?>
+    </p>
+    <p>Yes another paragraph.</p>
+</body>
+</html>
